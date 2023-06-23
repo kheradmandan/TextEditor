3531bbb2dd14de753f3e7e32da95549a7a22e782
faeze kheradmand <fa.kheradmand@gmail.com>
2023-06-23 12:05:54 +0330

    test

--- Editor/Program.cs
+++ Editor/Program.cs
@@ -10,7 +10,7 @@ namespace Editor
     {
         /// <summary>
         /// The main entry point for the application.
-        /// </summary>
+        /// </summary>//
         [STAThread]
         static void Main()
         {
